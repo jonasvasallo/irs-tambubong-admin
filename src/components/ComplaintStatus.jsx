cb73014ed7607af08c3173280f877539e77b3855
Jonas Vasallo <iderrillix@gmail.com>
2024-06-19 09:04:00 +0800

    update: complaints module handle 3 schedules

--- src/components/ComplaintStatus.jsx
+++ src/components/ComplaintStatus.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { doc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
 import { firestore } from "../config/firebase";
 
 const ComplaintStatus = (props) => {
     const [status, setStatus] = useState('');
     const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+  const [dismissalReason, setDismissalReason] = useState("");
 
   const saveStatus = async () => {
     setError('');
@@ -15,10 +16,33 @@ const ComplaintStatus = (props) => {
     }
 
     try{
+      if(props.complainant && props.respondent && status == "Dismissed"){
+        if(!dismissalReason){
+          setError("Please add a dismissal reason first!");
+          return;
+        }
+        const complainantDocRef = doc(firestore, "users", props.complainant);
+        const complainantNotifRef = collection(complainantDocRef, "notifications");
+        await addDoc(complainantNotifRef, {
+          title: "Your complaint has been dismissed",
+          content: `Your complaint got dismissed. Reason: ${dismissalReason}.`,
+          timestamp: serverTimestamp(),
+        })
+        const respondentDocRef = doc(firestore, "users", props.respondent);
+        const respondentNotifRef = collection(respondentDocRef, "notifications");
+        await addDoc(respondentNotifRef, {
+          title: 'Complaint was dismissed',
+          content: 'The complaint that you had received has been dismissed. If you have any questions please submit a support ticket.',
+          timestamp: serverTimestamp(),
+        })
+      }
+
+      console.log("this happened why");
       const complaintDocRef = doc(firestore, "complaints", props.id);
       await updateDoc(complaintDocRef, {
         status: status.trim(),
       });
+      
       setSuccess("Status updated successfully");
       setError('');
       window.location.reload();
@@ -33,8 +57,11 @@ const ComplaintStatus = (props) => {
         <select name="" id="" className="dropdown" onChange={(e) => setStatus(e.target.value)}>
           <option value="" selected disabled>Select a status</option>
             <option value="Open">Open</option>
+            <option value="In Progress">In Progress</option>
             <option value="Closed">Closed</option>
+            <option value="Dismissed">Dismissed</option>
         </select>
+        {(status == "Dismissed") ? <textarea name="" id="" className='multi-line' rows={5} placeholder='Reason for dismissal' style={{marginTop: '16px'}} onChange={(e) => setDismissalReason(e.target.value)}></textarea> : <></>}
         <br />
         <br />
         {success && <span className='status success'>{success}</span>}
--- src/pages/complaints/ComplaintDetailsPage.jsx
+++ src/pages/complaints/ComplaintDetailsPage.jsx
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from 'react'
 import Sidebar from '../../components/Sidebar'
 import Header from '../../components/Header'
 import { useParams } from 'react-router-dom'
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { firestore } from '../../config/firebase'
 import { useModal } from '../../core/ModalContext';
 import Modal from '../../components/Modal';
+import moment from 'moment'
 
 import '../../styles/complaintpage.css';
 import ComplaintStatus from '../../components/ComplaintStatus'
@@ -16,6 +17,8 @@ const ComplaintDetailsPage = () => {
     const {id} = useParams();
     const [ComplaintDetails, setComplaintDetails] = useState();
     const [ScheduleDetails, setScheduleDetails] = useState();
+    const [hearingHistory, setHearingHistory] = useState([]);
+    const [limit, setLimit] = useState(false);
     useEffect(() => {
         const fetchComplaintDetials = async () => {
             const complaintDocRef = doc(firestore, "complaints", id);
@@ -25,11 +28,42 @@ const ComplaintDetailsPage = () => {
 
                 setComplaintDetails(complaintData);
 
-                if(complaintData.schedule_id){
-                    const scheduleDocRef = doc(firestore, "schedules", complaintData.schedule_id);
-                    const scheduleSnapshot = await getDoc(scheduleDocRef);
+                // if(complaintData.schedule_id){
+                //     const scheduleDocRef = doc(firestore, "schedules", complaintData.schedule_id);
+                //     const scheduleSnapshot = await getDoc(scheduleDocRef);
 
-                    setScheduleDetails(scheduleSnapshot.data());
+                //     setScheduleDetails(scheduleSnapshot.data());
+                // }
+                if(complaintData.hearings){
+                    if(complaintData.hearings.length < 3){
+                    } else{
+                        setLimit(true);
+                    }
+                    const allHearings = complaintData.hearings;
+                    const hearingHistoryDetails = [];
+                    for(let i =0; i < allHearings.length; i++){
+                        const scheduleDocRef = doc(firestore, "schedules", allHearings[i].schedule_id);
+                        const scheduleSnapshot = await getDoc(scheduleDocRef);
+
+                        hearingHistoryDetails.push({...scheduleSnapshot.data(), status: allHearings[i].status});
+                    }
+                    setHearingHistory(hearingHistoryDetails);
+                    const latestSchedule = complaintData.hearings[complaintData.hearings.length - 1];
+                    const scheduleDocRef = doc(firestore, "schedules", latestSchedule.schedule_id);
+                    const scheduleSnapshot = await getDoc(scheduleDocRef);
+                    const meeting_end_date = new Date(moment(scheduleSnapshot.data().meeting_end.toDate()).format("YYYY-MM-DDTHH:mm"));
+                    const current_date = new Date();
+                    if(meeting_end_date < current_date){
+                        allHearings[complaintData.hearings.length - 1].status = "Done";
+                        await updateDoc(complaintDocRef, {
+                        hearings: allHearings,
+                        });
+                    } else {
+                        setScheduleDetails(scheduleSnapshot.data());
+                    }
+                    
+                    
+                    
                 }
             } catch (error){
                 console.log(error);
@@ -39,6 +73,18 @@ const ComplaintDetailsPage = () => {
 
         fetchComplaintDetials();
     }, []);
+
+    const closeCase = async ()=>{
+        const complaintDocRef = doc(firestore, "complaints", id);
+        try{
+            await updateDoc(complaintDocRef, {
+                status: 'Closed',
+            })
+            window.location.reload();
+        } catch(error){
+            alert(error.message);
+        }
+    }
   return (
     <div className="content">
         <Sidebar />
@@ -61,18 +107,32 @@ const ComplaintDetailsPage = () => {
                                         <span><span className="subheading-m">Issued at: <span className="body-m">{new Date(ComplaintDetails.issued_at.seconds * 1000).toLocaleString()}</span></span></span>
                                         <div className="flex gap-8">
                                             <span className="status error">{ComplaintDetails.status}</span>
-                                            <button className="button text" onClick={() => openModal("Update Status", "", <ComplaintStatus id={id}/>, 'info', <></>)}>Change</button>
+                                            <button className="button text" onClick={() => openModal("Update Status", "", <ComplaintStatus id={id} complainant={ComplaintDetails.issued_by} respondent={ComplaintDetails.respondent_id}/>, 'info', <></>)}>Change</button>
                                         </div>
                                     </div>
                                     <div className='flex-1'>
-                                        {(!ComplaintDetails.schedule_id) ? 
-                                        <button className="button filled" onClick={() => openModal("Schedule a meeting", "", <CreateSchedule id={id} complainant={ComplaintDetails.issued_by} respondent={ComplaintDetails.respondent_id}/>, 'info', <></>)}>Schedule</button> 
+                                        {limit && !ScheduleDetails && <button className='button filled' onClick={()=>closeCase()}>Close</button>}
+                                        {((ComplaintDetails.status != "Dismissed" && ComplaintDetails.status != "Closed") && (!ScheduleDetails && !limit)) ? 
+                                        <button className="button filled" onClick={() => openModal("Schedule Hearing", "", <CreateSchedule id={id} complainant={ComplaintDetails.issued_by} respondent={ComplaintDetails.respondent_id}/>, 'info', <></>)}>Schedule</button> 
                                         : 
                                         ScheduleDetails && <div className='status success textalign-start flex main-between gap-16'>
-                                        This complaint has been scheduled for a meeting on {new Date(ScheduleDetails.meeting_start.seconds * 1000).toLocaleString()}. 
+                                        This complaint has been scheduled for a hearing on {new Date(ScheduleDetails.meeting_start.seconds * 1000).toLocaleString()}. 
                                         <button className='button text' onClick={() => window.location.href = "/schedules"}>Update</button>
                                         </div>
                                         }
+                                    </div>
+                                </div>
+                                <div>
+                                    <span className="subheading-m">Hearing History:</span>
+                                    <div className="flex col gap-8">
+                                    {hearingHistory.map((history) => (
+                                        <div className='flex gap-16'>
+                                            <span>{history.title}</span>
+                                            <span>-</span>
+                                            <span>{moment.unix(history.meeting_start.seconds).format('ddd, MMMM D, YYYY [at] h:mm A')}</span>
+                                            <span>{history.status}</span>
+                                        </div>
+                                    ))}
                                     </div>
                                 </div>
                                 <br />
